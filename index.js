a6df69c52cc96420521dae971330ec246f139b4b
manish negi <manish.negi@dipoletechi.com>
2022-08-01 13:01:57 +0530

    node xommit

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
-console.log('hello world')
+const express = require("express");
 const http = require("http");
-const port = 3000;
+const port = 3002;
+const app = express();
+require('dotenv').config();
+console.log('hello world', process.env.APP_MODE)
 
 //Create HTTP server and listen on port 3000 for requests
-const server = http.createServer((req, res) => {
+// const server = http.createServer((req, res) => {
 
-  //Set the response HTTP header with HTTP status and Content type
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+//   //Set the response HTTP header with HTTP status and Content type
+//   res.statusCode = 200;
+//   res.setHeader('Content-Type', 'text/plain');
+//   res.end('Hello World\n');
+// });
+
+
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder applicatiodn."  + JSON.stringify(process.env)})
+})
 
 //listen for request on port 3000, and as a callback function have the port listened on logged
-server.listen(port, () => {
+app.listen(port, () => {
   console.log(`Server running at :${port}/`);
 });
