15c86da7ee84420d9ab30eabc7e7a7b05c9741cb
manish negi <manish.negi@dipoletechi.com>
2022-08-02 16:21:52 +0530

    sdf

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT;
 
 
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to my New Node App : " + process.env.API_URL + "  App Mode : " + process.env.APP_MODE})
+  res.json({ message: "Welcome to my New Node App : " + process.env.API_URL + "  App Mode : " + process.env.APP_MODE + " Node Env : " + process.env.NODE_ENV})
 })
 
 //listen for request on port 3000, and as a callback function have the port listened on logged
